0393514a61d42aba0508d915a34ee4cb28fb2e44
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-10-07 10:41:13 +0200

    Updated library

--- Assets/Lib/DryWetMidi/DryWetMidi.Tests.Common/MidiDevicesNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Melanchall.DryWetMidi.Tests.Common
-{
-    public static class MidiDevicesNames
-    {
-        public const string DeviceA = "MIDI A";
-        public const string DeviceB = "MIDI B";
-        public const string DeviceC = "MIDI C";
-    }
-}
